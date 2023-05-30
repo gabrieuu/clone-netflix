1db6e70e678e61c4f290ab6180ad6fe14b022af7
gabrieuu <mcgabriel208@gmail.com>
2023-05-30 12:07:45 -0300

    modificações no site

--- clone-netflix/src/components/FeaturedMovie.jsx
+++ clone-netflix/src/components/FeaturedMovie.jsx
@@ -23,16 +23,17 @@ export default ({item}) => {
                <div className="featured-box">
                     <div className="featured-name">{item.original_name}</div>
                     <div className="featured-info">
-                        <div className="featured-points margin-right"><span> {item.vote_average.toFixed(2)} </span> pontos</div>
+                        <div className="featured-points margin-right">{item.vote_average.toFixed(2)} pontos</div>
                         <div className="featured-year margin-right">{firstDate.getFullYear()}</div>
                         <div className="featured-seasons">{item.number_of_seasons} temporadas</div>
                     </div>
                     <div className="featured-description">{item.overview}</div>
-                    <div className="featured-genres">{genres.join(", ")}</div>
+                    
                     <div className="featured-buttons">
-                        <a href="#">Assistir</a>
-                        <a href="#">Minha Lista</a>
+                        <a className="watch" href="#">Assistir</a>
+                        <a className="myList" href="#">Minha Lista</a>
                     </div>
+                    <div className="featured-genres">{genres.join(", ")}</div>
                 </div>
             </div>
         </section>
--- clone-netflix/src/components/RowFilms.jsx
+++ clone-netflix/src/components/RowFilms.jsx
@@ -1,11 +1,24 @@
 import React from "react";
 import './RowFilms.css';
+import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos';
+import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
 export default ({title,items}) => {
+    const arrowLeft = () => {
+
+    }
+    const arrowRight = () => {
+        
+    }
     return(
         <div className="movieRow">
             <h2>{title}</h2>
+            
             <div className="movieRow-listarea">
-
+            <a className="seta left" onClick={arrowLeft}><ArrowBackIosIcon style={{
+                fontSize:40,
+                transform:'translateX(20%)'
+                }}/></a>
+            <a className="seta right" onClick={arrowRight}><ArrowForwardIosIcon style={{fontSize:40}}/></a>
                 <div className="movieRow-list">
                 {items.results.length > 0 && items.results.map((iten, key)=>( 
                     <div key ={key} className="movieRow-item">
