fa0a972344a2eb2cbda37c364001206e20afd4c6
gabrieuu <mcgabriel208@gmail.com>
2023-06-01 00:14:11 -0300

    navBar mobile

--- clone-netflix/src/components/Navbar.jsx
+++ clone-netflix/src/components/Navbar.jsx
@@ -1,15 +1,20 @@
-import React from "react";
+import { useState } from "react";
 import './NavBar.css'
 import logo from '../images/netflix_logo.png'
 import avatar from '../images/avatar.png'
 import MenuIcon from '@mui/icons-material/Menu';
 export default () => {
+
+    const [menu,setMenu] = useState(false)
+
     return (
         <nav>
+            <div className="bar">
+            
+            <div className="navegacao">
             <div className="logo">
                 <img src={logo} alt="" />
             </div>
-            <div className="navegacao">
                 <ul className="lista">
                     <a href="#"><li>Início</li></a>
                     <a href="#"><li>Séries</li></a>
@@ -18,16 +23,19 @@ export default () => {
                     <a href="#"><li>Minha lista</li></a>
                     <a href="#"><li>Navegar por idiomas</li></a>
                 </ul>
-                <div className="menu-hamburguer">
-               <MenuIcon style={{fontSize:40, color:'#e5e5e5'}}/> 
-                </div>
+
+               
             </div>
             <div className="config">
                 <div className="avatar">
                     <img src={avatar} alt="" />
                 </div>
+                <div className="menu-hamburguer">
+                    <MenuIcon style={{fontSize:40, color:'#e5e5e5'}}/> 
+                </div>
+            </div>  
             </div>
-            
+                     
         </nav>
     )
 }
