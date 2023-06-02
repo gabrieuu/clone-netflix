38c363bcaf39baa789e081d36354d6b2beb3c4a5
gabrieuu <mcgabriel208@gmail.com>
2023-06-02 11:55:10 -0300

    fix: navBar

--- clone-netflix/src/components/Navbar.jsx
+++ clone-netflix/src/components/Navbar.jsx
@@ -11,29 +11,29 @@ export default () => {
         <nav>
             <div className="bar">
             
-            <div className="navegacao">
-            <div className="logo">
-                <img src={logo} alt="" />
-            </div>
-                <ul className="lista">
-                    <a href="#"><li>Início</li></a>
-                    <a href="#"><li>Séries</li></a>
-                    <a href="#"><li>Filmes</li></a>
-                    <a href="#"><li>Bombando</li></a>
-                    <a href="#"><li>Minha lista</li></a>
-                    <a href="#"><li>Navegar por idiomas</li></a>
-                </ul>
+                <div className="navegacao">
+                    <div className="logo">
+                        <img src={logo} alt="" />
+                    </div>
+                    <ul className="lista">
+                        <a href="#"><li>Início</li></a>
+                        <a href="#"><li>Séries</li></a>
+                        <a href="#"><li>Filmes</li></a>
+                        <a href="#"><li>Bombando</li></a>
+                        <a href="#"><li>Minha lista</li></a>
+                        <a href="#"><li>Navegar por idiomas</li></a>
+                    </ul>
 
-               
-            </div>
-            <div className="config">
-                <div className="avatar">
-                    <img src={avatar} alt="" />
+                
                 </div>
-                <div className="menu-hamburguer">
-                    <MenuIcon style={{fontSize:40, color:'#e5e5e5'}}/> 
-                </div>
-            </div>  
+                <div className="config">
+                    <div className="avatar">
+                        <img src={avatar} alt="" />
+                    </div>
+                    <div className="menu-hamburguer">
+                        <MenuIcon style={{fontSize:40, color:'#e5e5e5'}}/> 
+                    </div>
+                </div>  
             </div>
                      
         </nav>
